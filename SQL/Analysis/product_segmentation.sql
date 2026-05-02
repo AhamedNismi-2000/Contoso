2d71f4467de7870dee790d3e8f879394a54eac6c
Ahamed Nismi <mnanismi2000@gmail.com>
2026-05-02 14:59:40 +0530

    Most Revenue Product

--- SQL/Analysis/product_segmentation.sql
+++ SQL/Analysis/product_segmentation.sql
@@ -1,2 +1,23 @@
 
+
 SELECT * FROM Product  
+
+-- Product Total Count Per Category 
+
+SELECT 
+    categoryname,
+    COUNT(categoryname) AS total_count
+FROM product
+GROUP BY categoryname
+
+
+-- Most Revenue Product 
+ SELECT * FROM Sales 
+
+    SELECT 
+        p.categoryname,
+        ROUND(SUM(s.quantity*s.exchangerate*s.unitprice),2) AS net_revenue 
+    FROM Product p 
+    JOIN Sales s
+    ON p.productkey = s.productkey 
+    GROUP BY p.categoryname
